f3cb52c66e1c4a3777d502e564a300b84c9549cc
t2t2 <taavot@gmail.com>
2017-09-15 23:15:34 +0300

    Temporary (tm) message for weekend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,15 @@
 		</div>
 		<div class="hero-body">
 			<div class="container">
+				<article class="message is-warning">
+					<div class="message-header">
+						<p><em>Wooooooooooooosh---</em> <a href="https://giphy.com/gifs/thetick-the-tick-3o7qiQwkN8El1GcePe" target="_blank">Bam!</a></p>
+					</div>
+					<div class="message-body">
+						Ooph that was quite a rough landing. While we're reattaching bits and pieces that fell off, you can already start using dctv v3!
+					</div>
+				</article>
+
 				<h1 class="title is-1">Welcome To The Diamond Club</h1>
 				<p class="subtitle is-2">Why not watch something live...</p>
 
