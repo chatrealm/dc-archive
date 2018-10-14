d1e8b33c5d26235124c83fe2da4276eec171d00f
t2t2 <taavot@gmail.com>
2018-10-14 20:04:23 +0300

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.fullpage')
+
+@section('content')
+	<div class="container">
+		<div class="columns">
+			<div class="column is-half is-offset-one-quarter has-text-centered">
+				<h1 class="title is-1">404</h1>
+				<h2 class="subtitle is-3">Ey where the page at</h2>
+			</div>
+		</div>
+	</div>
+@endsection
